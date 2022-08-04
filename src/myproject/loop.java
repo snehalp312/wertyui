1177a119f9b2ead3796c8d00440ddbbc9f77a0a5
snehalp312 <110602774+snehalp312@users.noreply.github.com>
2022-08-04 03:27:56 -0700

    Add files via upload

---
+++ src/myproject/loop.java
@@ -0,0 +1,26 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//odd no
+		int n,i, Sum=0;
+		Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the rang:-");
+	n= sc.nextInt();
+	i=1;
+	While("i<=n");
+	{
+		int C=i*2
+				
+	}
+	
+	
+
+	}
+
+}
